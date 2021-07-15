1ad06ecc3993e786d45bdacc716226cf1c39e881
Mohit Jha <55483009+Mohit-17@users.noreply.github.com>
2021-07-15 12:40:34 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+//add this file to .gitignore
+module.exports = {
+    google: {
+        clientID:'751600208207-cum1ndkc11djst2d2deve8i53o46fdar.apps.googleusercontent.com',
+        clientSecret:'KSWPocoSPiaGfObj5I7b1TF6'
+    },
+    mongodb: {
+        dbURI:'mongodb+srv://mohit:softeng24@cluster0.a9lln.mongodb.net/Dance-Website'
+    },
+    session:{
+        cookieKey:'mywebsiteisawesomeiguess'
+    }
+};  
